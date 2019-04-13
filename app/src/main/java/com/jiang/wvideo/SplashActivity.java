e9ebff8d6f325924c27173079073be890266190f
wwwjf <wwwjf2015@outlook.com>
2019-04-13 22:41:28 +0800

    Initial commit

---
+++ app/src/main/java/com/jiang/wvideo/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.jiang.wvideo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 启动页
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
